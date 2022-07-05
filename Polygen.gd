ffe1821868ab57f36dd4006cfa94f332152cf193
Michael Reel <mykilr@gmail.com>
2022-07-05 22:27:08 +0100

    Trying to add material. Currently needs re-applied on tool draw

--- Polygen.gd
+++ Polygen.gd
@@ -34,12 +34,13 @@ func create_geometry() -> void:
 	
 	var root = create_base(a, width)
 	
-	var branch_length = 1.0
-	var branch_spread = 1.0
-	var sub_branches = 4
-	var limit = 3
+	var branch_length := 1.0
+	var branch_spread := 1.0
+	var sub_branches := 4
+	var limit := 3
+	var uv_switch := false
 	
-	create_branching(root, b, width, branch_length, branch_spread, sub_branches, limit)
+	create_branching(root, b, width, branch_length, branch_spread, sub_branches, limit, uv_switch)
 
 
 func create_base(root_point: Vector3, base_width: float) -> PoolIntArray:
@@ -72,7 +73,8 @@ func create_branching(
 	length: float,
 	spread: float,
 	sub_branches: int,
-	limit: int
+	limit: int,
+	uv_switch: bool
 ) -> void:
 	if limit <= 0:
 		return
@@ -81,16 +83,16 @@ func create_branching(
 	var branch_rot_delta := 2.0 * PI / sub_branches
 	for _branch_index in range(sub_branches):
 		if limit <= 1:
-			create_twig(base_vertices, bifurication, width)
+			create_twig(base_vertices, bifurication)
 			continue
 			
-		var branch_root := create_branch(base_vertices, bifurication, width)
+		var branch_root := create_branch(base_vertices, bifurication, width, uv_switch)
 		var sub_bifurication := bifurication + Vector3(0, length, 0) + sub_branch_rot
 		sub_branch_rot = sub_branch_rot.rotated(Vector3.DOWN, branch_rot_delta)
-		create_branching(branch_root, sub_bifurication, width, length, spread, sub_branches, limit - 1)
+		create_branching(branch_root, sub_bifurication, width, length, spread, sub_branches, limit - 1, !uv_switch)
 
 
-func create_branch(base_vertices: PoolIntArray, bifurication: Vector3, width: float) -> PoolIntArray:
+func create_branch(base_vertices: PoolIntArray, bifurication: Vector3, width: float, uv_switch: bool) -> PoolIntArray:
 	var i : int = vertices.size()
 	vertices.append_array([
 		# These should probably be adjusted to account for line tilt, etc
@@ -107,16 +109,17 @@ func create_branch(base_vertices: PoolIntArray, bifurication: Vector3, width: fl
 		base_vertices[3], base_vertices[0], i + 0, base_vertices[3], i + 0, i + 3, # Quad between BACK and LEFT
 	])
 	# These are not going to be make sense for now
-	uvs.append_array([
-		Vector2.DOWN, Vector2.ONE, Vector2.ZERO, Vector2.RIGHT,
-	])
+	if uv_switch:
+		uvs.append_array([Vector2.DOWN, Vector2.ONE, Vector2.ZERO, Vector2.RIGHT])
+	else:
+		uvs.append_array([Vector2.ZERO, Vector2.RIGHT, Vector2.DOWN, Vector2.ONE])
 	normals.append_array([
 		Vector3.LEFT, Vector3.FORWARD, Vector3.RIGHT, Vector3.BACK,
 	])
 	return PoolIntArray(range(i, i + 4))
 
 
-func create_twig(base_vertices: PoolIntArray, bifurication: Vector3, width: float) -> void:
+func create_twig(base_vertices: PoolIntArray, bifurication: Vector3) -> void:
 	var i : int = vertices.size()
 	vertices.append_array([bifurication])
 	# We're not really giving much head to rotation here, should be okay for the moment
